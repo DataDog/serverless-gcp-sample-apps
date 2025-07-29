92b3c6a1e42886cb6e589a8578af66f52c62247d
Nicholas Hulston <nicholas.hulston@datadoghq.com>
2025-07-29 15:42:36 -0400

    add go sample app

---
+++ cloud-run/in-process/golang/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	httptrace "github.com/DataDog/dd-trace-go/contrib/net/http/v2"
+	dd_logrus "github.com/DataDog/dd-trace-go/contrib/sirupsen/logrus/v2"
+	"github.com/DataDog/dd-trace-go/v2/ddtrace/tracer"
+	"github.com/sirupsen/logrus"
+)
+
+const PORT = "8080"
+
+func main() {
+	tracer.Start()
+	defer tracer.Stop()
+	logrus.SetFormatter(&logrus.JSONFormatter{})
+	logrus.AddHook(&dd_logrus.DDContextLogHook{})
+
+	mux := httptrace.NewServeMux()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		// Use the request context for trace-log correlation
+		ctx := r.Context()
+		logrus.WithContext(ctx).Info("Hello World!")
+
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintf(w, "Hello World!")
+	})
+
+	logrus.Infof("Starting server on port %s", PORT)
+
+	if err := http.ListenAndServe(":"+PORT, mux); err != nil {
+		logrus.Fatalf("Server failed to start: %v", err)
+	}
+}
