fd56987d9bec3d8a05381c5be637e814e77e7bd4
Nicholas Hulston <nicholashulston@gmail.com>
2025-08-08 14:15:02 -0400

    Optimize Images & Add Python log/trace correlation (#6)

--- cloud-run/in-process/python/app.py
+++ cloud-run/in-process/python/app.py
@@ -1,12 +1,30 @@
+from ddtrace import tracer
 from flask import Flask
+import structlog
+import sys
+
+def tracer_injection(logger, log_method, event_dict):
+    event_dict.update(tracer.get_log_correlation_context())
+    return event_dict
+
+structlog.configure(
+    processors=[
+        tracer_injection,
+        structlog.processors.EventRenamer("msg"),
+        structlog.processors.JSONRenderer()
+    ],
+    logger_factory=structlog.WriteLoggerFactory(file=sys.stdout),
+)
+
+logger = structlog.get_logger()
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    print("Hello world!")
+    logger.info("Hello world!")
     return 'Hello World!', 200
 
 if __name__ == '__main__':
-    print("starting server on port 8080")
+    logger.info("starting server on port 8080")
     app.run(host='0.0.0.0', port=8080)
